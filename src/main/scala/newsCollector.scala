d9cdf227c226fda97e45a1f5b902a2cb206d8b76
Stephane KATTOOR <s.kattoor@linkbynet.com>
2018-03-27 10:34:50 +0200

    Lazily download of news items
    Only selected content is now lazily fetched from the internet, in preparation for using a cache for articles

--- src/main/scala/newsCollector.scala
+++ src/main/scala/newsCollector.scala
@@ -13,8 +13,11 @@ import com.rometools.rome.feed.synd._
 import com.rometools.rome.io.SyndFeedInput
 import com.rometools.rome.io.XmlReader
 
-case class NewsSource(title: String, uri: String)
-case class NewsItem(source: NewsSource, title: String, uri: String, content: String)
+class NewsSource(val title: String, val uri: String)
+class NewsItem(val source: NewsSource, val title: String, val uri: String, contentSource: => String) {
+	lazy val content = contentSource
+}
+
 
 object Main extends App {
 	val input = new SyndFeedInput();
@@ -24,13 +27,13 @@ object Main extends App {
 	)
 	def getItems(feedUrl: String) = {
 		val feed = input.build(new XmlReader(new URL(feedUrl)));
-		val source = NewsSource(feed.getTitle, feed.getUri)
+		val source = new NewsSource(feed.getTitle, feed.getUri)
 		feed.getEntries.toList.par map { e => 
-			val content = Jsoup.connect(e.getLink).get.html
-			NewsItem(source, e.getTitle, e.getLink, content) 
+			lazy val content = { println(s"Fetching ${e.getLink}"); Jsoup.connect(e.getLink).get.html }
+			new NewsItem(source, e.getTitle, e.getLink, content) 
 		}
 	}
-	sources flatMap getItems foreach { x => println(
+	sources flatMap getItems filter { _.title.toLowerCase.matches(".*intell.*") } foreach { x => println(
 s"""---	
 ${x.title}
 ${x.uri}
